fe80c6e291e3c87297f86930fec71590b97da37f
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-26 19:54:48 -0300

    created assets access

---
+++ src/constants/assets.js
@@ -0,0 +1,14 @@
+import { ReactComponent as PlusIcon } from "../images/icon-plus.svg";
+import { ReactComponent as MinusIcon } from "../images/icon-minus.svg";
+
+import { ReactComponent as ReplyIcon } from "../images/icon-reply.svg";
+import { ReactComponent as EditIcon } from "../images/icon-edit.svg";
+import { ReactComponent as DeleteIcon } from "../images/icon-delete.svg";
+
+export default {
+  PlusIcon,
+  MinusIcon,
+  ReplyIcon,
+  EditIcon,
+  DeleteIcon
+}
