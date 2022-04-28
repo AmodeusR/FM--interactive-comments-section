df1399a32785b8800ba5d116a453d1d000810d95
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-28 11:29:36 -0300

    Updated Comments with data

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,12 +1,24 @@
 // import React from "react";
 import { Comment } from "../../components";
+import data from "../../../data.json";
 
 import "./comments.scss";
 
 const Comments = () => {
+  const comments = data.comments;
   return (
     <div className="comments">
-      <Comment />
+      {comments.map(({ id, content, createdAt, score, user, replies }) => (
+        <Comment
+          key={id}
+          username={user.username}
+          userPicture={user.image.webp}
+          commentBody={content}
+          score={score}
+          createdAt={createdAt}
+          replies={replies}
+        />
+      ))}
     </div>
   );
 };
