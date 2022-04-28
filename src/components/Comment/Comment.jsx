60a53ebe0004f1b607eaa821489e21f07a97dd35
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-28 11:30:08 -0300

    Updated Comment with data & replies

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,30 +1,52 @@
-import "./comment.scss";
+import { Replies, ConditionalContainer } from "../../components";
 import { assets } from "../../constants";
+import "./comment.scss";
 
 const { PlusIcon, MinusIcon, ReplyIcon } = assets;
 
-const Comment = () => {
-  
+const Comment = ({
+  username,
+  userPicture,
+  createdAt,
+  commentBody,
+  score = 0,
+  replies = [],
+  replyingTo,
+  isReplies = false
+}) => {
   return (
-    <div className="comment">
+    <ConditionalContainer
+      condition={isReplies === false}
+      container={(children) => <div className="comment-container">{children}</div>}
+    >
+      <div className="comment">
         <div className="comment__user-info">
-          <img src="https://source.unsplash.com/random/100x100" alt="user picture" className="comment__picture" />
-          <span className="comment__username">Amodeus R.</span>
-          <span className="comment__timestamp">1 hour ago</span>
+          <img
+            src={userPicture}
+            alt="user picture"
+            className="comment__picture"
+          />
+          <span className="comment__username">{username}</span>
+          <span className="comment__timestamp">{createdAt}</span>
         </div>
-      <p className="comment__body">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum placeat beatae sequi quasi fuga quo officiis sed, eligendi eos. Consequatur quos veritatis saepe enim, laudantium ipsa adipisci fugiat quas natus?
-      </p>
-      <div className="comment__score">
-        <PlusIcon className="comment__icon" />
-        <span className="comment__score-count">0</span>
-        <MinusIcon className="comment__icon" />
+        <p className="comment__body">
+          {replyingTo ? <span className="comment__reply-quote">@{replyingTo} </span> : ""}
+          {commentBody}
+        </p>
+        <div className="comment__score">
+          <PlusIcon className="comment__icon" />
+          <span className="comment__score-count">{score}</span>
+          <MinusIcon className="comment__icon" />
+        </div>
+        <button className="comment__reply-button">
+          <ReplyIcon className="comment__reply-button-icon" />
+          Reply
+        </button>
       </div>
-      <button className="comment__reply">
-        <ReplyIcon className="comment__reply-icon" />
-        Reply
-      </button>
-    </div>
+      {replies.length ?
+        <Replies replies={replies} /> : ""
+      }
+    </ConditionalContainer>
   );
 };
 
